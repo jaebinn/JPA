206a299ecd617624fa2c0f1f1c6c2a9c291fdab2
jaebinn <whiteclover129@gmail.com>
2024-07-04 17:29:42 +0900

    페이징처리

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,11 +21,9 @@ public class QuestionController {
 
 
     @GetMapping("list")
-    public String list(Model model){
-        //List<Question> questionList = this.questionRepository.findAll();
-        List<Question> questionList = this.questionService.getList();
-        System.out.println("질문: "+questionList);
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page){
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "questionList";
     }
 
