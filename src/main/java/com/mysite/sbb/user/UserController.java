ccd2d1a8a8285f7c3255dfe9408ec82302d5451b
jaebinn <whiteclover129@gmail.com>
2024-07-05 14:26:14 +0900

    유저 회원가입 유효성검사

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -1,8 +1,8 @@
 package com.mysite.sbb.user;
 
 import jakarta.validation.Valid;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -20,19 +20,34 @@ public class UserController {
 
     @GetMapping("signup")
     public String signup(UserCreateForm userCreateForm){
-        return "signup";
+        return "signupForm";
     }
 
-    @PostMapping("signup")
-    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    @PostMapping("/signup")
+    public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "signupForm";
         }
-        if(userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
-            bindingResult.rejectValue("password2","passwordIncorrect","2개의 패스워드가 일치하지 않습니다.");
+
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
             return "signupForm";
         }
-        userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        //create할 떄 같은 데이터가 있는지 예외처리
+        try {
+            userService.create(userCreateForm.getUsername(),
+                    userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            //bindingResult.reject(오류코드, 오류메시지) -> 일반적인 오류메시지를 등록할 때 사용한다.
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signupForm";
+        } catch (Exception e){
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signupForm";
+        }
 
         return "redirect:/";
     }
