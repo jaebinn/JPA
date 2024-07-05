168f7bb33d47093267bb5b9faa71bbadd6d4d948
jaebinn <whiteclover129@gmail.com>
2024-07-05 17:34:52 +0900

    앵커(페이지가 긴 문서에서 특정부분 클릭하면 그 위치로 이동) 기능 추가

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -37,9 +37,13 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "questionDetail";
         }
-        this.answerService.create(question, answerForm.getContent(),siteUser);
+        Answer answer = this.answerService.create(question,
+                answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
+        //this.answerService.create(question, answerForm.getContent(),siteUser);
         //Todo: 답변을 저장한다.
-        return String.format("redirect:/question/detail/%s", id);
+        //return String.format("redirect:/question/detail/%s", id);
 
     }
 
@@ -66,7 +70,8 @@ public class AnswerController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
         this.answerService.answerModify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -86,6 +91,7 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",
+                answer.getQuestion().getId(), answer.getId());
     }
 }
